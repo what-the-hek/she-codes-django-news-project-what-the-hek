1e2b79827d8423ae13ce07c0611180d928d2e69a
what-the-hek <108735805+what-the-hek@users.noreply.github.com>
2022-12-13 19:02:12 +0800

    edited css

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -13,7 +13,6 @@ class CreateAccountView(CreateView):
 class ViewAccountView(generic.DetailView):
 	model = CustomUser
 	template_name = 'users/viewAccount.html'
-	# success_url = reverse_lazy('users:viewAccount')
 	success_url = reverse_lazy('login')
 
 class AllAccountsView(generic.DetailView):
