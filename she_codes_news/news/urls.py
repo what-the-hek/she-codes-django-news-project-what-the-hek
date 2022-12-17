37e5793f4a22c589a5ea3dc2887909d492dd06dc
what-the-hek <108735805+what-the-hek@users.noreply.github.com>
2022-12-17 13:03:11 +0800

    added edit and delete function for stories

--- she_codes_news/news/urls.py
+++ she_codes_news/news/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.StoryView.as_view(), name='story'),
     path('add-story/', views.AddStoryView.as_view(), name='newStory'),
-    path('user/<int:pk>/', views.AuthorStories.as_view(), name='authorStories')
+    path('<int:pk>/edit-story', views.EditStoryView.as_view(), name='editStory'),
+    path('<int:pk>/delete-story', views.DeleteStoryView.as_view(), name='deleteStory'),
+    path('delete-success', views.delete_success_view, name='deleteSuccess'),
+    path('user/<int:pk>/', views.AuthorStories.as_view(), name='authorStories'),
 ]
--- she_codes_news/news/views.py
+++ she_codes_news/news/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse_lazy
 from .models import NewsStory
 from .forms import StoryForm
 from users.models import CustomUser
+from django.shortcuts import render
 
 
 class IndexView(generic.ListView):
@@ -29,12 +30,47 @@ class AddStoryView(generic.CreateView):
     template_name = 'news/createStory.html'
     success_url = reverse_lazy('news:index')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['add_story_view'] = True
+        return context
+
     def form_valid(self, form):
         # this means to set the author as the user currently logged in
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-        
+class EditStoryView(generic.edit.UpdateView):
+    form_class = StoryForm
+    model = NewsStory
+    context_object_name = 'storyForm'
+    template_name = 'news/createStory.html'
+    success_url = reverse_lazy('news:index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['add_story_view'] = False
+        return context
+
+    def get_queryset(self):
+        return NewsStory.objects.all()
+
+def delete_success_view(request):
+    return render(request, 'news/deleteSuccess.html')
+    # model = NewsStory
+    # template_name = 'news/deleteSuccess.html'
+    # success_url = reverse_lazy('news:index')
+
+class DeleteStoryView(generic.edit.DeleteView):
+    model = NewsStory
+    template_name = 'news/deleteView.html'
+    success_url = reverse_lazy('news:deleteSuccess')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['story'] = NewsStory.objects.get(id=self.kwargs['pk'])
+        return context
+
 # new class for viewing authors stories
 class AuthorStories(generic.ListView):
     template_name = 'news/authorStories.html'
